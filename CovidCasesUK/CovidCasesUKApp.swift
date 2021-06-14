8b22acf2799d6e37d7dd78efeea140ac763e16a7
Riccardo Rizzo <riccardo.rizzo@machinemax.com>
2021-06-14 16:17:01 +0100

    Initial Commit

---
+++ CovidCasesUK/CovidCasesUKApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidCasesUKApp.swift
+//  CovidCasesUK
+//
+//  Created by Riccardo Rizzo on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct CovidCasesUKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
